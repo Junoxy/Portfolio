f922b35a7b023da8cd3193c219c78de2da3da4be
Junoxy <junjun.privated@gmail.com>
2024-05-05 13:19:21 +0200

    try fix vercel image path

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -12,12 +12,12 @@ const HomePage = ({theme}) => {
         <Box className='home-text-container'>
             <Typography className="home-text1" sx={{fontWeight:'300',fontSize:'66px',color:theme.text, marginBottom:'-45px', marginLeft: '15px'}} >Juno</Typography>
             <Typography className="home-text2" sx={{fontWeight:'400',fontSize:'90px',color:theme.accent, marginLeft: '70px'}} >Ogay</Typography>
-            <Avatar className='home-img' src="./Images/swaglord.jpg" alt="Juno" sx={{width: '300px', height: '300px', display:{xs:'block', md:'none'}}}/>
+            <Avatar className='home-img' src="/images/swaglord.jpg" alt="Juno" sx={{width: '300px', height: '300px', display:{xs:'block', md:'none'}}}/>
             <Divider className='home-divider' sx={{width:'150%', marginLeft:'-65px',bgcolor: theme.primary,display:{xs:'none', md:'block'}}}/>
             <Typography className="home-text3" sx={{fontWeight:'300',fontSize:'66px',color:theme.text, marginBottom:'-30px', marginLeft: {xs:'60px',md:'-45px'}}} >Web</Typography>
             <Typography className="home-text4" sx={{fontWeight:'700',fontSize:'72px',color:theme.accent}} >Developer</Typography>
         </Box>
-            <Avatar className='home-img' src="./Images/swaglord.jpg" alt="Juno" sx={{width: '400px', height: '400px', display:{xs:'none', md:'block'}}}/>
+            <Avatar className='home-img' src="/images/swaglord.jpg" alt="Juno" sx={{width: '400px', height: '400px', display:{xs:'none', md:'block'}}}/>
         </Box>
     )
 } 
